ba1d5d795478f9f18e4eb4d356cbcd5283440fe3
guykem123 <50095264+guykem123@users.noreply.github.com>
2020-06-23 17:58:00 +0300

    update

--- src/groupBarScript.js
+++ src/groupBarScript.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    setDataLabelInnerHtml()
     setFontSize()
     window.onresize = setFontSize;
 })
@@ -7,20 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
 function setFontSize() {
     const dataLabel = document.querySelector("g[ngx-charts-series-horizontal]")
     const bar = dataLabel.querySelector("g[ngx-charts-bar]")
-    const height = bar.getAttribute("ng-reflect-height")
+    const height = bar.getBoundingClientRect().height
     const dataLabels = document.querySelectorAll(".textDataLabel")
     for (let i = 0; i < dataLabels.length; i++) {
         const element = dataLabels[i]
-        element.style.fontSize = height > 20 ? height > 40 ? height > 60 ? height / 4
-            : height / 3
-            : height / 2
-            : height / 1.5
-    }
-}
-function setDataLabelInnerHtml() {
-    const dataLabels = document.querySelectorAll(".textDataLabel")
-    for (let i = 0; i < dataLabels.length; i++) {
-        const element = dataLabels[i]
-        element.innerHTML = element.innerHTML + "&nbsp;"
+        element.style.fontSize = height - 5
+        console.log(height)
     }
 }
